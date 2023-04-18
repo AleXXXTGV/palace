47f94d53ccde0d374d34299ecdd5e9e915cf50bd
AleXXXTGV <alekstagaev17@gmail.com>
2023-04-18 15:40:33 +0500

    header bug fixes

--- scripts/index.js
+++ scripts/index.js
@@ -4,3 +4,9 @@ burger.onclick = () => {
     burger.classList.toggle('burger_active')
     document.querySelector('.header-mobile__modal').classList.toggle('header-mobile__modal_active')
 }
+
+let languages = document.querySelector('.languages')
+languages.onclick = () => {
+    languages.classList.toggle('languages_active')
+}
+
